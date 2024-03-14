41379bf9dc1622679bfd1c602ca61475b432135b
iggzq <2811328244@qq.com>
2024-03-14 23:18:23 +0800

    项目初始化,搭建架构

--- user_mod/src/main/java/com/forum/user/controller/UserController.java
+++ user_mod/src/main/java/com/forum/user/controller/UserController.java
@@ -1,5 +1,8 @@
 package com.forum.user.controller;
 
+import org.ltz.result.CommonResult;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -12,7 +15,11 @@ import org.springframework.web.bind.annotation.RestController;
  * @since 2024-03-14 10:17:52
  */
 @RestController
-@RequestMapping("/user")
+@RequestMapping("/user/")
 public class UserController {
 
+    @GetMapping("login")
+    public CommonResult<String> login(){
+        return CommonResult.success("登录成功");
+    }
 }
