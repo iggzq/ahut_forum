49cf71d527145486dd3dbbf4e6f9208f47a5ba7f
iggzq <2811328244@qq.com>
2024-03-14 23:18:23 +0800

    项目初始化,搭建架构

---
+++ common/src/main/java/org/ltz/result/CommonResult.java
@@ -0,0 +1,47 @@
+package org.ltz.result;
+
+import lombok.Data;
+
+@Data
+public class CommonResult<T> {
+    //返回码
+    private Integer code;
+    //返回描述
+    private String desc;
+    //返回的数据
+    private T data;
+
+    public CommonResult() {
+    }
+
+    public CommonResult(Integer code, String desc, T data) {
+        this.code = code;
+        this.desc = desc;
+        this.data = data;
+    }
+    public static <T> CommonResult<T> success(T data) {
+        return new CommonResult<T>(200, "success", data);
+    }
+
+    public static <T> CommonResult<T> success() {
+        return new CommonResult<T>(200, "success", null);
+    }
+
+    public static <T> CommonResult<T> failed() {
+        return new CommonResult<T>(500, "failed", null);
+    }
+
+    public static <T> CommonResult<T> failed(String msg) {
+        return new CommonResult<T>(500, msg, null);
+    }
+
+    public static <T> CommonResult<T> failed(Integer code, String msg) {
+        return new CommonResult<T>(code, msg, null);
+    }
+
+
+
+
+
+
+}
--- user_mod/src/main/java/com/forum/user/run/UserApplication.java
+++ user_mod/src/main/java/com/forum/user/run/UserApplication.java
@@ -1,12 +1,16 @@
 package com.forum.user.run;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 /**
  * Hello world!
  */
 @SpringBootApplication
+@ComponentScan("com.forum.user")
+@MapperScan("com.forum.user.mapper")
 public class UserApplication {
     public static void main(String[] args) {
         SpringApplication springApplication = new SpringApplication(UserApplication.class);
