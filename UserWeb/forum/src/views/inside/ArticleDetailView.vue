b1c098b24870a4beed45624b0499e6e421604e71
iggzq <2811328244@qq.com>
2024-03-20 17:01:11 +0800

    前端评论发送界面

--- UserWeb/forum/src/views/inside/ArticleDetailView.vue
+++ UserWeb/forum/src/views/inside/ArticleDetailView.vue
@@ -1,12 +1,12 @@
 <template>
+  <van-sticky>
+    <van-nav-bar left-arrow title="文章详情">
+      <template #left>
+        <van-icon name="arrow-left" @click="goBack"/>
+      </template>
+    </van-nav-bar>
+  </van-sticky>
   <div class="animate__animated animate__fadeInRight">
-    <van-sticky>
-      <van-nav-bar left-arrow title="文章详情">
-        <template #left>
-          <van-icon name="arrow-left" @click="goBack"/>
-        </template>
-      </van-nav-bar>
-    </van-sticky>
     <div class="animate__animated  articleShow">
       <var-card
         class="articleDetailShow"
@@ -33,12 +33,62 @@
           </van-button>
         </template>
       </var-card>
-
+    </div>
+    <div class="commentShow">
+      <var-card class="commentDetailShow">
+        <template #description>
+          <van-skeleton :row="3" class="commentSkeleton" title/>
+        </template>
+      </var-card>
     </div>
   </div>
+  <div class="bottomArea">
+    <div class="bottomContent">
+      <van-field
+        v-model="writeComment.comment"
+        autosize
+        maxlength="50"
+        placeholder="请输入留言"
+        readonly
+        rows="2"
+        show-word-limit
+        type="textarea"
+        @click="showWriteComment"
+      />
+      <div class="bottomCommentIcon">
+        <van-button round type="success">
+          <van-icon name="guide-o" size="24px" @click="sendComment"/>
+        </van-button>
+      </div>
+    </div>
+  </div>
+  <var-popup v-model:show="bottom" :default-style="true" position="bottom">
+    <van-nav-bar title="留言">
+      <template #left>
+        <van-icon name="cross" size="25" @click="bottom = false"/>
+      </template>
+      <template #right>
+        <van-button style="height: 30px" type="primary" @click="sendComment">发布</van-button>
+      </template>
+    </van-nav-bar>
+    <van-cell-group inset>
+      <van-field
+        v-model="writeComment.comment"
+        :autofocus="true"
+        autosize
+        class="writeCommentContent"
+        maxlength="100"
+        placeholder="请输入留言"
+        rows="2"
+        show-word-limit
+        type="textarea"
+      />
+    </van-cell-group>
+  </var-popup>
 </template>
 <script>
-import { defineComponent, ref } from 'vue'
+
+import { defineComponent, nextTick, ref, watch } from 'vue'
 import { useStore } from 'vuex'
 import axios from 'axios'
 import { showSuccessToast } from 'vant'
@@ -51,6 +101,11 @@ export default defineComponent({
     const articleDetail = store.state.articleDetail
     const buttonColor = ref('#afb0b2')
     const likeAnimate = ref('')
+    const comment = ref('')
+    const bottom = ref(false)
+    const writeComment = ref({
+      comment: ''
+    })
     const goBack = () => {
       window.history.go(-1)
     }
@@ -67,13 +122,48 @@ export default defineComponent({
         }
       })
     }
+    const sendComment = () => {
+      axios.post('http://172.20.10.3:8081/article/commentArticle', {
+        articleId: articleDetail.id,
+        comment: writeComment.value.comment
+      }).then(res => {
+        if (res.data.code === 200) {
+          bottom.value = false
+          writeComment.value.comment = ''
+          articleDetail.commentCount += 1
+          showSuccessToast('评论成功')
+        }
+      })
+    }
+    const showWriteComment = () => {
+      console.log(123)
+      bottom.value = true
+    }
+    watch(() => bottom.value,
+      (newVal) => {
+        if (newVal) {
+          nextTick(() => {
+            const textarea = document.querySelector('.writeCommentContent textarea')
+            if (textarea) {
+              textarea.focus()
+            }
+          })
+        }
+      },
+      { immediate: true }
+    )
     console.log(articleDetail)
     return {
       articleDetail,
       goBack,
       sendLikeArticle,
       buttonColor,
-      likeAnimate
+      likeAnimate,
+      comment,
+      sendComment,
+      showWriteComment,
+      bottom,
+      writeComment
     }
   }
 })
@@ -118,10 +208,55 @@ export default defineComponent({
   color: darkgrey;
 }
 
+.commentShow {
+  width: 100%;
+  display: flex;
+  margin-top: 10px;
+  justify-content: space-around;
+}
+
+.commentDetailShow {
+  margin-top: 5px;
+  width: 95%;
+}
+
+.commentSkeleton {
+  margin-top: 10px;
+}
+
 .itemPopular {
   width: 100%;
   text-align: right;
   margin: 0px;
   color: darkgrey;
+}
+
+.bottomArea {
+  position: fixed;
+  bottom: 0px;
+  width: 100%;
+  height: 50px;
+  background-color: white;
+}
+
+.bottomContent {
+  height: 100%;
+  display: flex;
+  justify-content: space-around;
+}
+
+.bottomCommentInput {
+  width: 83%;
+  height: 85%;
+}
+
+.bottomCommentIcon {
+  display: flex;
+  flex-direction: column;
+  justify-content: space-around;
+}
+
+.writeCommentContent{
+  height: 50vh;
 }
 </style>
