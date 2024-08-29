283e0cc6ef2d880b3b4c2861c20d409af7a92016
iggzq <2811328244@qq.com>
2024-08-29 21:51:44 +0800

    fix(forum): 修正聊天室URL格式并更新Vue组件语法修正了.env.development中聊天室URL的格式，确保其以ws协议开头，以解决连接问题。同时，更新了ArticleView.vue中的文本格式，使其在渲染帖子信息时更加自然。
    此外，对MenuView.vue组件进行了重构，使用了script setup语法，简化了组件的定义并提升了代码的可维护性。
    路由守卫和tabbar隐藏逻辑已调整，以适应新的语法结构。

--- UserWeb/forum/src/views/MenuView.vue
+++ UserWeb/forum/src/views/MenuView.vue
@@ -1,31 +1,24 @@
 <template>
   <router-view/>
-  <van-tabbar  v-if="!isTabbarHidden" v-model="active" route>
-    <van-tabbar-item icon="home-o" to="/article">文章</van-tabbar-item>
+  <van-tabbar v-if="!isTabbarHidden" v-model="active" route>
+    <van-tabbar-item icon="home-o" to="/article" @click="handleIsFresh">文章</van-tabbar-item>
     <van-tabbar-item icon="manager-o" to="/myProfile">我的</van-tabbar-item>
   </van-tabbar>
 </template>
-<script>
-import { computed, defineComponent, onMounted } from 'vue'
+<script setup>
+import { computed, onMounted } from 'vue'
 import { useRouter } from 'vue-router'
 import { useStore } from 'vuex'
 
-export default defineComponent({
-  setup () {
-    const router = useRouter()
-    const store = useStore()
-    const active = store.state.activeTab
-    const isTabbarHidden = computed(() => {
-      return router.currentRoute.value.meta.hideTabbar
-    })
-    onMounted(() => {
-      router.push(active === 0 ? '/article' : '/myProfile')
-    })
-    return {
-      active,
-      isTabbarHidden
-    }
-  }
+const router = useRouter()
+const store = useStore()
+const active = store.state.activeTab
+const isTabbarHidden = computed(() => {
+  return router.currentRoute.value.meta.hideTabbar
+})
+
+onMounted(() => {
+  router.push(active === 0 ? '/article' : '/myProfile')
 })
 </script>
 <style scoped>
